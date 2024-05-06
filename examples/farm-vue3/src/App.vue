295e9cfa203acf4102bf60dc80c72513765790f3
erkelost <1256029807@qq.com>
2024-05-06 20:36:14 +0800

    chore: update example

--- examples/farm-vue3/src/App.vue
+++ examples/farm-vue3/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div text-center font-sans p4>
+    <div logo />
     <div text-red bg-blue text-lg>
       Hello UnoCSS
     </div>
+    <div i-custom-icon />
+    <div i-custom-multi-line-attr />
+    <!-- @unocss-skip-start -->
+    <div class="bg-[url(../src/uno.svg)] bg-[url(/uno.svg)]" />
+    <!-- @unocss-skip-end -->
   </div>
 </template>
